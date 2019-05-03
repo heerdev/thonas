aad29572e62952d6e7cf115d308f6bfae581925e
zubair <zubair@heersoftware.com>
2019-05-03 17:07:30 +0530

    manual components

--- thonas-ui/src/app/app.component.ts
+++ thonas-ui/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'thonas-ui';
+export class AppComponent implements OnInit{
+  title = 'Avengers - end game begins';
+
+  subTitle =  'Awesome Angular!';
+
+  constructor() {};
+
+  getSubtitle(): string {
+     return this.subTitle;
+
+  }
+
+  ngOnInit(): void {
+  }
+
 }
--- thonas-ui/src/app/app.module.ts
+++ thonas-ui/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HomePageComponent} from './home-page/home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
